053b0daf9fdae8c562bce9bcbef243b6bedda461
srushtikhanolkar286-droid <srushtikhanolkar286@gmail.com>
2026-09-19 22:22:23 +0530

    Add files via upload

---
+++ bsfindinelementsortedrotated.java
@@ -0,0 +1,30 @@
+public class bsfindinelementsortedrotated {
+    public int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length -1;
+        while(low<=high){
+            int mid = low +(high - low)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=target && arr[mid]>=target){
+                    high = mid -1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(arr[high]>=target && arr[mid]<=target){
+                    low = mid+1;
+                }
+                else{
+                    high = mid -1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
